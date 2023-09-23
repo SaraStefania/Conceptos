98f94561dcb1cbf8f6e7d69e10669c6ca4dd2162
sara040616 <sara.sierraramos@gmail.com>
2023-09-23 12:31:30 -0500

    feat: se agrego funcionalidad para manejar el vento del contenedor que almacenara las palabras

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-const palabra = document.getElementsByClassName('palabra');
+const word = document.getElementsByClassName('palabra');
 const span = document.getElementsByClassName('container-span');
 
-for(i = 0; i < palabra.length; i++){
-palabra[i].addEventListener('dragstart', (e) =>{
+for(i = 0; i < word.length; i++){
+word[i].addEventListener('dragstart', (e) =>{
     e.dataTransfer.setData('text/plain', e.target.id)
 })
 }
+
 for(i = 0; i < span.length; i++ ){
-    span[i].addEventListener("dragover", (ev) => {
-        console.log("dragOver");
-        ev.preventDefault();
-      });
+    span[i].addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+
+    span[i].addEventListener('drop', (e) =>{
+        e.preventDefault();
+        const data = e.dataTransfer.getData("text/plain");
+        const source = document.getElementById(data);
+        e.target.appendChild(source);
+    })
 }
 
