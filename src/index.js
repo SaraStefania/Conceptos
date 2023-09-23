976d9a475c8d72b5946fe9cd21716f1c89ca2072
sara040616 <sara.sierraramos@gmail.com>
2023-09-23 12:15:24 -0500

    feat: funcionalidad para prevenir el vento del contenedor de destino

--- src/index.js
+++ src/index.js
@@ -6,5 +6,10 @@ palabra[i].addEventListener('dragstart', (e) =>{
     e.dataTransfer.setData('text/plain', e.target.id)
 })
 }
-
+for(i = 0; i < span.length; i++ ){
+    span[i].addEventListener("dragover", (ev) => {
+        console.log("dragOver");
+        ev.preventDefault();
+      });
+}
 
