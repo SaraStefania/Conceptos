2924c3c609cf1050444c865c59e761ff5962561a
sara040616 <sara.sierraramos@gmail.com>
2023-09-24 17:02:07 -0500

    feat: se agrego la funcionalidad para comprobar si la palabra esta en la ubicación correcta

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
 const word = document.getElementsByClassName('palabra');
 const span = document.getElementsByClassName('container-span');
+const comprobarBtn = document.getElementById('comprobarBtn');
 
-for(i = 0; i < word.length; i++){
-word[i].addEventListener('dragstart', (e) =>{
-    e.dataTransfer.setData('text/plain', e.target.id)
-})
+let originalContainer = null;
+
+
+for (let i = 0; i < word.length; i++) {
+  word[i].addEventListener('dragstart', (e) => {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    originalContainer = e.target.parentNode; 
+  });
 }
 
-for(i = 0; i < span.length; i++ ){
-    span[i].addEventListener('dragover', (e) => {
+for (let i = 0; i < span.length; i++) {
+  span[i].addEventListener('dragover', (e) => {
+    if (e.target.children.length === 0 ) {
         e.preventDefault();
-    });
+      }
+  });
 
-    span[i].addEventListener('drop', (e) =>{
-        e.preventDefault();
-        const data = e.dataTransfer.getData("text/plain");
-        const source = document.getElementById(data);
-        e.target.appendChild(source);
-    })
+  span[i].addEventListener('drop', (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+    const source = document.getElementById(data);
+    e.target.appendChild(source);
+  });
 }
+
+comprobarBtn.addEventListener('click', () => {
+    let correctCount = 0;
+
+    for (let i = 0; i < span.length; i++) {
+        const rightWord = span[i].getAttribute('data-word').toLowerCase();
+        const spanChildren = span[i].children;
+
+        if (spanChildren.length === 1) {
+            const slurredWord = spanChildren[0].textContent.toLowerCase();
+            
+            if (slurredWord === rightWord ) {
+               alert('correct')
+            } else {
+                originalContainer.appendChild(spanChildren[0]);
+            }
+        }
+    }
+});
 
